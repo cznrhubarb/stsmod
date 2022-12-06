dd23c141be469f715d7a66d50874e97bc41a88ec
Jen Pelham <jenpelm@gmail.com>
2022-12-05 19:44:55 -0500

    add card art

--- src/main/java/musclemantot/cards/skill/Sunbathe.java
+++ src/main/java/musclemantot/cards/skill/Sunbathe.java
@@ -1,5 +1,6 @@
 package musclemantot.cards.skill;
 
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -21,12 +22,21 @@ public class Sunbathe extends BaseCard {
 
     public static final String ID = makeID(cardInfo.baseId);
 
+    private static final int BLOCK = 6;
+    private static final int UPG_BLOCK = 2;
+
+
     public Sunbathe() {
         super(cardInfo);
+
+        setBlock(BLOCK, UPG_BLOCK);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
+        public void use(AbstractPlayer p, AbstractMonster m) {
+            addToBot(new GainBlockAction(p, p, block));
+        }
     }
 
     @Override
