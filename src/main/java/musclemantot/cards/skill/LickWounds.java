705c6c4d11de862cedf75af1cddd353ea2a70471
Jen Pelham <jenpelm@gmail.com>
2022-12-05 21:09:23 -0500

    add stuff

--- src/main/java/musclemantot/cards/skill/LickWounds.java
+++ src/main/java/musclemantot/cards/skill/LickWounds.java
@@ -1,10 +1,14 @@
 package musclemantot.cards.skill;
 
+import com.megacrit.cardcrawl.actions.common.HealAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
 import musclemantot.cards.BaseCard;
 import musclemantot.characters.MuscleManTot;
+import musclemantot.util.BingeUtil;
 import musclemantot.util.CardInfo;
 
 import static musclemantot.MuscleManTotMod.makeID;
@@ -12,7 +16,7 @@ import static musclemantot.MuscleManTotMod.makeID;
 public class LickWounds extends BaseCard {
     private final static CardInfo cardInfo = new CardInfo(
             LickWounds.class.getSimpleName(),
-            1,
+            2,
             CardType.SKILL,
             CardTarget.SELF,
             CardRarity.COMMON,
@@ -21,12 +25,52 @@ public class LickWounds extends BaseCard {
 
     public static final String ID = makeID(cardInfo.baseId);
 
+    private static final int BINGE_THRESHOLD = 3;
+    private static final int HEAL = 7;
+    private static final int UPG_HEAL = 3;
+
     public LickWounds() {
         super(cardInfo);
+
+        setMagic(BINGE_THRESHOLD);
+        setMagic(HEAL, UPG_HEAL);
+        setExhaust(true);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
+        if (AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT) {
+            AbstractDungeon.getCurrRoom().smoked = true;
+
+        }
+
+        this.addToBot(new HealAction(p, p, this.magicNumber));
+    }
+
+    @Override
+    public boolean canUse(AbstractPlayer p, AbstractMonster m) {
+        boolean canUse = super.canUse(p, m);
+        if (!canUse) {
+            return false;
+        } else {
+            if (BingeUtil.getPlayerBinge(false) < BINGE_THRESHOLD) {
+                this.cantUseMessage = cardStrings.EXTENDED_DESCRIPTION[0];
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }
+
+    @Override
+    public void triggerOnGlowCheck() {
+        boolean glow = BingeUtil.getPlayerBinge(false) >= BINGE_THRESHOLD;
+
+        if (glow) {
+            this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
+        } else {
+            this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
+        }
     }
 
     @Override
--- src/main/java/musclemantot/cards/skill/SlowBlink.java
+++ src/main/java/musclemantot/cards/skill/SlowBlink.java
@@ -1,8 +1,16 @@
 package musclemantot.cards.skill;
 
+import com.evacipated.cardcrawl.mod.stslib.actions.common.StunMonsterAction;
+import com.evacipated.cardcrawl.mod.stslib.powers.StunMonsterPower;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.StrengthPower;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
 import musclemantot.cards.BaseCard;
 import musclemantot.characters.MuscleManTot;
 import musclemantot.util.CardInfo;
@@ -12,21 +20,28 @@ import static musclemantot.MuscleManTotMod.makeID;
 public class SlowBlink extends BaseCard {
     private final static CardInfo cardInfo = new CardInfo(
             SlowBlink.class.getSimpleName(),
-            1,
+            2,
             CardType.SKILL,
-            CardTarget.SELF,
+            CardTarget.ENEMY,
             CardRarity.COMMON,
             MuscleManTot.Enums.CARD_COLOR
     );
 
     public static final String ID = makeID(cardInfo.baseId);
 
+    private static final int STUN = 1;
+
     public SlowBlink() {
         super(cardInfo);
+
+        setMagic(STUN);
+        setExhaust(true);
+        setEthereal(true, false);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new StunMonsterAction(m,p,1));
     }
 
     @Override
